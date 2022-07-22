c04beb79cee5c09ab40c743fa96ed1d9201f964d
Vinícius Lourenço <vinyygamerlol@gmail.com>
2022-07-21 22:08:11 -0300

    chore(vite): added missing configuration to coverage work well

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
     /* for example, use global to avoid globals imports (describe, test, expect): */
     // globals: true,
   },
-})
+});
